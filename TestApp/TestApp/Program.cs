cfd67eb1b471f0750a7c7134b37bfe23e8ed45a9
Superhack52 <krasilse@gmail.com>
2015-08-21 11:49:19 +0300

    Вторая глава книги
    Проба рефакторинга и класс диаграмм

---
+++ TestApp/TestApp/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConfigureCUI();
+            // Ожидать нажатия <Enter>
+            Console.ReadLine();
+        }
+
+        private static void ConfigureCUI()
+        {
+            // Настроить консольный интерфейс (CUI)
+            Console.Title = "My Rocking App";
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.WriteLine("***********************************");
+            Console.WriteLine("**** Welcome to My Rocking App ****");
+            Console.WriteLine("***********************************");
+            Console.BackgroundColor = ConsoleColor.Black;
+        }
+    }
+}
