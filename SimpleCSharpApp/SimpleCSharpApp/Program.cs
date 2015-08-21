592c3666979f740988228b1a2ea215e458493c90
Superhack52 <krasilse@gmail.com>
2015-08-21 14:12:35 +0300

    Третья глава
    1 программа. работа с args

---
+++ SimpleCSharpApp/SimpleCSharpApp/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleCSharpApp
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            // Вывести пользователю простое сообщение
+            Console.WriteLine("***** My Firsrt C# App *****");
+            Console.WriteLine("Hello World!");
+            Console.WriteLine();
+
+            //Ожидания нажатия кнопки Enter
+            Console.ReadLine();
+
+            // Обрабатываем любые входные аргументы
+            for (int i = 0; i < args.Length; i++)
+                Console.WriteLine("Arg: {0}", args[i]);
+            Console.ReadLine();
+
+            // Обработать любые входящие аргументы с помощью foreach
+            foreach (string arg in args)
+                Console.WriteLine("Arg: {0}",arg);
+            Console.WriteLine();
+
+            // Получить аргументы с использованием System.Environment
+            String[] theArgs = Environment.GetCommandLineArgs();
+            foreach (string arg in theArgs)
+                Console.WriteLine("Arg: {0}",arg);
+            Console.ReadLine();
+            //Возращает произвольный код ошибки
+            return -1;
+        }
+    }
+}
